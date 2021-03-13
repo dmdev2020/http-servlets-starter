eb3eef08012a9eccc753b6fc23100d64b20c6efd
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-03-13 17:12:35 +0300

    added code from lesson-27

---
+++ src/com/dmdev/http/servlet/FirstServlet.java
@@ -0,0 +1,32 @@
+package com.dmdev.http.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (var writer = resp.getWriter()) {
+            writer.write("<h1>Hello from First Servlet</h2>");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
