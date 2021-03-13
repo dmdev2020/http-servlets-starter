a924a61929c14a2541db73775173d3916dc66c5f
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-03-13 17:48:48 +0300

    added code from lesson-28

--- src/com/dmdev/http/servlet/FirstServlet.java
+++ src/com/dmdev/http/servlet/FirstServlet.java
@@ -19,9 +19,17 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
+        var headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            var header = headerNames.nextElement();
+            System.out.println(req.getHeader(header));
+        }
+
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setHeader("token", "12345");
+//        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
         try (var writer = resp.getWriter()) {
-            writer.write("<h1>Hello from First Servlet</h2>");
+            writer.write("<h1>Привет с первого сервлета!</h2>");
         }
     }
 
