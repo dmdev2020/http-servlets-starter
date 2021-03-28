21edc87813f198cb186011ec41a198e931ac060a
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-03-28 08:57:07 +0300

    added code from lesson-35

---
+++ src/com/dmdev/http/servlet/CookieServlet.java
@@ -0,0 +1,40 @@
+package com.dmdev.http.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@WebServlet("/cookies")
+public class CookieServlet extends HttpServlet {
+
+    private static final String UNIQUE_ID = "userId";
+    private final AtomicInteger counter = new AtomicInteger();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        var cookie = req.getHeader("cookie");
+        var cookies = req.getCookies();
+        if (cookies == null || Arrays.stream(cookies)
+                .filter(cookie -> UNIQUE_ID.equals(cookie.getName()))
+                .findFirst()
+                .isEmpty()) {
+            var cookie = new Cookie(UNIQUE_ID, "1");
+            cookie.setPath("/cookies");
+            cookie.setMaxAge(15 * 60);
+            resp.addCookie(cookie);
+            counter.incrementAndGet();
+        }
+
+        resp.setContentType("text/html");
+        try (var writer = resp.getWriter()) {
+            writer.write(counter.get());
+        }
+    }
+}
