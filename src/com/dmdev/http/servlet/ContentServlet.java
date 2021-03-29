c786e80683bbe4164722945e838ac551c39b6518
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-03-29 13:36:49 +0300

    added code from lesson-42

--- src/com/dmdev/http/servlet/ContentServlet.java
+++ src/com/dmdev/http/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package com.dmdev.http.servlet;
 
+import com.dmdev.http.dto.FlightDto;
+import com.dmdev.http.service.FlightService;
 import com.dmdev.http.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,12 +10,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
+
+import static java.util.stream.Collectors.toMap;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
 
+    private final FlightService flightService = FlightService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<FlightDto> flightDtos = flightService.findAll();
+        req.setAttribute("flights", flightDtos);
+        req.getSession().setAttribute("flightsMap", flightDtos.stream()
+                .collect(toMap(FlightDto::getId, FlightDto::getDescription)));
+
         req.getRequestDispatcher(JspHelper.getPath("content"))
                 .forward(req, resp);
     }
--- web/WEB-INF/jsp/content.jsp
+++ web/WEB-INF/jsp/content.jsp
@@ -6,7 +6,17 @@
 <body>
     <%@ include file="header.jsp"%>
     <div>
-        Content. Русский
+        <span>Content. Русский</span>
+        <p>Size: ${requestScope.flights.size()}</p>
+        <p>Id: ${requestScope.flights.get(0).id}</p>
+        <p>Id 2: ${requestScope.flights[1].id}</p>
+        <p>Map Id 2: ${sessionScope.flightsMap[1]}</p>
+        <p>JSESSION id: ${cookie["JSESSIONID"].value}, unique identifier</p>
+        <p>Header: ${header["Cookie"]}</p>
+        <p>Param id: ${param.id}</p>
+        <p>Param test: ${param.test}</p>
+        <p>Empty list: ${not empty flights}</p>
+<%--        don't use variables without scope--%>
     </div>
     <%@ include file="footer.jsp"%>
 </body>
